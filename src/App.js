256b1d2a2f325b305f5c5cc3c4fb43bccbbd0932
Babar-thedeveloper <babarkhursheed345@gmail.com>
2025-02-01 14:40:41 +0500

    first push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route, } from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import LoginSignup from './Pages/LoginSignup';
+import Home from './Pages/Home';
+import Footer from './Components/Footer/Footer';
+import mens_banner from '..//src//Components//Assets//banner_mens.png'
+import women_banner from '..//src//Components//Assets//banner_women.png'
+import kid_banner from '..//src//Components//Assets//banner_kids.png'
 function App() {
   return (
     <div>
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/mens' element={<ShopCategory banner={mens_banner} category="men" />} />
+          <Route path='/womens' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
+          <Route path=':productId' element={<Product />} />
+          <Route path='/Cart' element={<Cart />} />
+          <Route path='/LoginSignup' element={<LoginSignup />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import logo from '..//Assets//logo.png'
+import cart_icon from '..//Assets/cart_icon.png'
+import { Link } from 'react-router-dom'
+
 const Navbar = () => {
-  return (
-    <div>
-      i a, a Navbar
-    </div>
-  )
+    const [menu, setMenu] = useState("home");
+
+
+    return (
+        <div className='navbar'>
+            <div className='nav_logo'>
+                <img src={logo} alt="" />
+                <p>VistaMart</p>
+            </div>
+            <ul className='nav_menu'>
+                <li onClick={() => { setMenu("home") }}> <Link style={{ textDecoration: 'none' , color:'rgb(57, 56, 56)'}} to={"/"}>Home</Link>{menu === "home" ? <hr></hr> : <></>}</li>
+                <li onClick={() => { setMenu("mens") }}> <Link style={{ textDecoration: 'none' , color:'rgb(57, 56, 56)'}} to={"/mens"}>Men</Link>{menu === "mens" ? <hr></hr> : <></>}</li>
+                <li onClick={() => { setMenu("women") }}><Link style={{ textDecoration: 'none' , color:'rgb(57, 56, 56)'}} to={"/womens"}>Women</Link>{menu === "women" ? <hr></hr> : <></>}</li>
+                <li onClick={() => { setMenu("kids") }}> <Link style={{ textDecoration: 'none' , color:'rgb(57, 56, 56)'}} to={"/kids"}>Kids</Link>{menu === "kids" ? <hr></hr> : <></>}</li>
+            </ul>
+            <div className="nav_login_cart">
+                <Link to={"/LoginSignup"}><button>Login</button></Link>
+                <Link to={"/LoginSignup"}><img src={cart_icon} alt="" /></Link>
+                <div className="cart_count">0</div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
